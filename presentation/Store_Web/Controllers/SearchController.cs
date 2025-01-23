81181f534858c6451c7af044aca04d8e35c4136c
andrei154992 <andrei21.klei@gmail.com>
2025-01-24 02:33:21 +0400

    Добавить страницу поиска

---
+++ presentation/Store_Web/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store;
+
+namespace Store_Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepositiry bookRepositiry;
+
+        public SearchController(IBookRepositiry bookRepositiry)
+        {
+            this.bookRepositiry = bookRepositiry;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = bookRepositiry.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
